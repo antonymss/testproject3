36240f8731eee729b408b44030fc865481579b43
antonymss <demeshkevichanton@gmail.com>
2021-06-10 16:15:51 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import './App.css';
+import {Header} from "./Header/Header";
+import {Profile} from './Profile';
+import {CreatePost, IFormInput} from "./CreatePost";
+import {Post} from "./Post";
+import {Page404} from './Page404/Page404';
+
+const initialState: Array<IFormInput> = []
+export type initialStateType = typeof initialState
+
+const reducer = (state: initialStateType = initialState, action: any) => {
+    switch (action.type) {
+        case 'createPost':
+            return {...state, newPost: state.push()};
+
+        default:
+            return state
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <div className="App">
+            <Header/>
+            <Switch>
+                <Route exact path='/' render={() => <Profile/>}/>
+                <Route path='/createPost' render={() => <CreatePost/>}/>
+                <Route path='/post' render={() => <Post/>}/>
+                <Route path={'/404'} render={() => <Page404/>}/>
+                <Redirect from={'*'} to={'/404'}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
