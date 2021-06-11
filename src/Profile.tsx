d652270d8617d06cc0d78c96a23fa818d7a10a2e
antonymss <demeshkevichanton@gmail.com>
2021-06-11 17:01:43 +0300

    refact

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import {initialStateType} from "./App";
 
 
-export const Profile = () => {
+export const Profile = ({posts}:{posts: initialStateType}) => {
+    const titleView = posts.map(post => <li>{post.title}</li>)
+    const textView = posts.map(post => <li>{post.text}</li>)
+    return <ul>
+        {titleView}
+        {textView}
 
-
-    return <div>
-        Пока нету постов
-
-    </div>
+    </ul>
 }
